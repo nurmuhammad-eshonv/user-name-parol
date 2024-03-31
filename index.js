5bfaadd38724257db2b974e0c47ae7c58cae284c
davrsirius <davrsirius@gmail.com>
2024-03-31 10:13:27 +0500

    added the code

---
+++ index.js
@@ -0,0 +1,8 @@
+let login = prompt('loginni kiriting')
+let parol = prompt('parolmgizni kiriting');
+
+if(login == 'nurmuhammad' && parol == 'nuric'){
+    document.write('Home pagega xush kelibsiz')
+}else if('notogri parol'){
+    prompt('parolni qaytaddan kiriting')
+}
